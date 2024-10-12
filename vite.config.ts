8f948106d62e67f2071f4c76c045573c29e6527d
Weslei França <weslwifranca@gmail.com>
2024-10-12 15:08:23 -0300

    add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://gaspartransportes.com.br', // substitua pelo domínio do seu site
+      dynamicRoutes: [
+        '/',
+      ],
+    }),
+
+  ],
 })
