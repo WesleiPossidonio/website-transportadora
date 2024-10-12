7e5da87f266670b0f9dd39b408b9c307db3728e8
Weslei França <weslwifranca@gmail.com>
2024-10-12 11:54:27 -0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString(); // Cria um chunk por pacote da node_modules
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 600, // Ajuste o limite para avisos de chunk
-  },
 })
