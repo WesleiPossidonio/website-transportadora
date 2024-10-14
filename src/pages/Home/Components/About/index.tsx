8a41d206ca650233709ea380d863fab5246523d3
Weslei França <weslwifranca@gmail.com>
2024-10-14 17:57:17 -0300

    update about

--- src/pages/Home/Components/About/index.tsx
+++ src/pages/Home/Components/About/index.tsx
@@ -28,17 +28,17 @@ export const About = () => {
     <ContainerAbout id="about">
       <ContainerTextAbout data-aos="fade-right" data-aos-duration="1000">
         <TitleText size="s">Sobre Nós</TitleText>
-        <TitleText color="text" id='title'>
+        <TitleText color="text" id="title">
           A força e a experiência que movem suas entregas
         </TitleText>
 
         <TextRegular size="s" className="TextAbout">
-          Nossa história é marcada por comprometimento e excelência.
+          Nossa história é marcada por comprometimento e excelência. {" "}
           <strong>
             Com anos de experiência, somos especialistas em oferecer as soluções
             logísticas ideais
-          </strong>{' '}
-          para que suas entregas sejam ágeis e seguras.
+          </strong>{" "}
+          para que suas entregas sejam ágeis e seguras. {" "}
           <strong>
             Confiança, qualidade e dedicação estão no centro de tudo o que
             fazemos.
@@ -46,11 +46,11 @@ export const About = () => {
         </TextRegular>
 
         <TextRegular>
-          Cuidamos de cada detalhe.
+          Cuidamos de cada detalhe.{" "}
           <strong>
             Desde o atendimento personalizado até a entrega pontual
-          </strong>{' '}
-          estamos sempre ao seu lado, garantindo que
+          </strong>{" "}
+          estamos sempre ao seu lado, garantindo que{" "}
           <strong>
             suas expectativas sejam superadas em cada etapa do transporte.
           </strong>
